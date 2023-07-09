97e7d1027538418e0cb5ec5a4f535b9407c46e8c
Alexander Melnichenko <togpe22@gmail.com>
2023-07-09 12:26:57 +0300

    Add files via upload

---
+++ remove-chars/script.js
@@ -0,0 +1,26 @@
+var inputTextarea = document.getElementById("inputString");
+
+inputTextarea.addEventListener("keydown", function(event) {
+  if (event.ctrlKey && event.keyCode === 13) { // 13 is the key code for Enter
+    event.preventDefault(); // Prevent the default Enter key behavior
+    modifyAndCopy();
+  }
+});
+
+function modifyAndCopy() {
+  var input = document.getElementById("inputString").value;
+
+  var modifiedString = input.replace(/[\n]+/g, "").replace(/[0-9]/g, "");
+
+  var tempTextarea = document.createElement("textarea");
+  tempTextarea.value = modifiedString;
+  document.body.appendChild(tempTextarea);
+  tempTextarea.select();
+  document.execCommand("copy");
+  document.body.removeChild(tempTextarea);
+  document.getElementById("myText").textContent = ""
+
+  var myTextElement = document.getElementById("myText");
+  myTextElement.textContent = "";
+  myTextElement.textContent = "“" + modifiedString + "”\nhas been add to the clipboard";
+}
